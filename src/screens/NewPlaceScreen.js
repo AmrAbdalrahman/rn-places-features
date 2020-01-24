b91df27307ebe15f19305c00a218bf56b113354a
AmrAlmagic <amr.abdalrahman94@gmail.com>
2020-01-24 16:10:05 +0200

    working with redux with setup and example

--- src/screens/NewPlaceScreen.js
+++ src/screens/NewPlaceScreen.js
@@ -1,14 +1,62 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, ScrollView, TextInput, Button, Alert} from 'react-native';
+import Colors from '../constants/Colors';
+import * as placesActions from '../store/actions/places';
+import {useDispatch} from 'react-redux';
 
 const NewPlaceScreen = props => {
+
+    const [titleValue, setTitleValue] = useState('');
+
+    const dispatch = useDispatch();
+
+    const titleChangeHandler = text => {
+        //you could add validation
+        setTitleValue(text);
+    };
+
+    const savePlaceHandler = () => {
+        if (titleValue.trim().length < 3) {
+            Alert.alert('Alert', 'Title must be minimum 3 characters', 'Ok');
+            return;
+        }
+        dispatch(placesActions.addPlace(titleValue));
+        props.navigation.goBack();
+    };
+
     return (
-        <View>
-            <Text>NewPlaceScreen</Text>
-        </View>
+        <ScrollView keyboardShouldPersistTaps={'always'}>
+            <View style={styles.form}>
+                <Text style={styles.label}>Title</Text>
+                <TextInput style={styles.textInput}
+                           onChangeText={titleChangeHandler}
+                           value={titleValue}/>
+                <Button title={'Save Place'} color={Colors.primary} onPress={savePlaceHandler}/>
+            </View>
+        </ScrollView>
     );
 };
 
-const styles = StyleSheet.create({});
+
+NewPlaceScreen.navigationOptions = {
+    headerTitle: 'Add Place',
+};
+
+const styles = StyleSheet.create({
+    form: {
+        margin: 30,
+    },
+    label: {
+        fontSize: 18,
+        marginBottom: 15,
+    },
+    textInput: {
+        borderBottomColor: '#ccc',
+        borderBottomWidth: 1,
+        marginBottom: 15,
+        paddingVertical: 4,
+        paddingHorizontal: 2,
+    },
+});
 
 export default NewPlaceScreen;
