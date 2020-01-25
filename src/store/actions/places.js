18a94ccba687ea2b6527ff0ef4607af85fc7accc
AmrAlmagic <amr.abdalrahman94@gmail.com>
2020-01-25 17:38:10 +0200

    save title and name of image

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,8 +1,7 @@
 export const ADD_PLACE = 'ADD_PLACE';
 export const SET_PLACES = 'SET_PLACES';
-import RNFS from 'react-native-fs';
-import RNFetchBlob from 'rn-fetch-blob';
-
+import * as RNFS from 'react-native-fs';
+//import RNFetchBlob from 'rn-fetch-blob';
 
 import {insertPlace, fetchPlaces} from '../../helpers/db';
 
@@ -13,19 +12,40 @@ export const addPlace = (title, image) => {
 
         const fileName = image.split('/').pop();
 
-        //const newPath = RNFS.DocumentDirectoryPath + '/' + fileName;
+        const newPath = `${RNFS.DocumentDirectoryPath}/${fileName}`;
 
-        const newPath = RNFetchBlob.fs.dirs.DocumentDir + '/' + fileName;
+        //const newPath = RNFetchBlob.fs.dirs.DocumentDir + '/' + fileName;
         console.log('new path', newPath);
         console.log('image', image);
 
-
         try {
-            //const fileMoved =await RNFS.moveFile(image, newPath);
-            const fileMoved = await RNFetchBlob.fs.createFile(newPath, image, 'uri');
+
+            /*const stat = await RNFetchBlob.fs.stat(image);
+            console.log('stat', stat);*/
+
+            /*const testDir = await RNFetchBlob.fs.mkdir(newPath);
+            console.log('testDir', testDir);*/
+
+            /*const fileMoved = await RNFetchBlob.fs.mv(image,newPath);
+
+         console.log('fileMoved', fileMoved);*/
+
+
+            /////////////
+
+
+            /*const stat = await RNFS.stat(image);
+            console.log('stat', stat);*/
+
+            /*  const testDir = await RNFS.mkdir('files');
+
+              console.log('testDir', testDir);*/
+
+            const fileMoved = await RNFS.moveFile(image, newPath);
+
+            const scaned = await RNFS.scanFile(newPath);
 
             console.log('fileMoved', fileMoved);
-
 
             const dbResult = await insertPlace(title, newPath, 'best address', 15.6, 12.3);
             dispatch({type: ADD_PLACE, placeDate: {id: dbResult.insertId, title: title, image: newPath}});
